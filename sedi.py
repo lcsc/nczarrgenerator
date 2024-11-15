afcfc03277b6731ea4cf2e3c8fde09b0a1cc8658
Eduardo Moreno <eduardo.moreno@ipe.csic.es>
2024-11-15 16:44:33 +0100

    Script for sedi

---
+++ sedi.py
@@ -0,0 +1,14 @@
+import warnings
+from nczarrgenerator import ncs2zarr
+
+warnings.filterwarnings('ignore')
+
+# [X] "Procesando: SEDI desde el fichero SEDI.nc"           time = 516; lat = 720 ; lon = 1440 => 'chunk_shape': (9, 90, 180)
+
+netcdfs = [
+    {'path': ['nc/sedi/SEDI.nc'], 'nc_var': 'SEDI', 'var': 'sedi', 'time_dim': 'time', 'lat_dim': 'lat', 'lon_dim': 'lon', 'nc_projection': 'EPSG:4326', 'calc_min_max': False, 'include_center_calc': True, 'chunk_shape': (9, 90, 180)},
+]
+zarr_path = 'nc/sedi.zarr'
+ncs2zarr(netcdfs, zarr_path)
+
+# 3s sin max/min (ya que si se activan los máximos y mínimos aparece un error por ser la unidad de time 'months since...')
