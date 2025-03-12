3888081deb4ebcdb46d39a0c1381d36cba606ab1
Eduardo Moreno <eduardo.moreno@ipe.csic.es>
2025-03-12 17:13:36 +0100

    sedi variable name change (upper)

--- sedi.py
+++ sedi.py
@@ -6,7 +6,7 @@ warnings.filterwarnings('ignore')
 # [X] "Procesando: SEDI desde el fichero SEDI.nc"           time = 516; lat = 720 ; lon = 1440 => 'chunk_shape': (9, 90, 180)
 
 netcdfs = [
-    {'path': ['nc/sedi/SEDI.nc'], 'nc_var': 'SEDI', 'var': 'sedi', 'time_dim': 'time', 'ver_dim': 'lat', 'hor_dim': 'lon', 'nc_projection': 'EPSG:4326', 'calc_min_max': False, 'include_center_calc': True, 'chunk_shape': (9, 90, 180)},
+    {'path': ['nc/sedi/SEDI.nc'], 'nc_var': 'SEDI', 'var': 'SEDI', 'time_dim': 'time', 'ver_dim': 'lat', 'hor_dim': 'lon', 'nc_projection': 'EPSG:4326', 'calc_min_max': False, 'include_center_calc': True, 'chunk_shape': (9, 90, 180)},
 ]
 zarr_path = 'nc/sedi.zarr'
 ncs2zarr(netcdfs, zarr_path)
